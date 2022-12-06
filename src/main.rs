7c9fa04c4b5af21087e28a6ddd0a902c90bf3f6b
James Timms <james.timms@wayhome.co.uk>
2022-12-06 13:29:14 +0000

    Create a simple rust microservice using actix

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{get, web, App, HttpRequest, HttpServer, Responder};
+
+#[get("/info")]
+async fn index(_req: HttpRequest) -> impl Responder {
+    "Hello from the info page."
+}
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
